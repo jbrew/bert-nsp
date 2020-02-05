e2a8b1f6568be2230e57272813fb15aefe0f7ba6
jbrew <brew.jamie@gmail.com>
2020-02-05 09:14:36 -0800

    candidate loop

--- runway_model.py
+++ runway_model.py
@@ -14,18 +14,24 @@ def setup(opts):
     tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
     return model, tokenizer
 
-
 @runway.command(name='sequence_score',
-               inputs={ 'sentence1': text(), 'sentence2': text()},
-               outputs={ 'score': number()})
+			   inputs={ 'line1': text(), 'line2_candidates': text()},
+			   outputs={ 'scores': list(number())})
 def sequence_score(setup_tuple, inputs):
-    model, tokenizer = setup_tuple
-    combined = inputs['sentence1'] + ' ' + inputs['sentence2']      # may be better to concatenate *after* tokenization using special [SEP] token
-    input_tokens = tokenizer.encode(combined, add_special_tokens=True)
-    input_ids = torch.tensor(input_tokens).unsqueeze(0)
-    outputs = model(input_ids)
-    seq_relationship_scores = outputs[0]     # outputs is an array with losses as the first value and logits as the second
-    return seq_relationship_scores.cpu().detach().numpy()
+	model, tokenizer = setup_tuple
+	line1 = inputs['line1']
+	line2_candidates = [line.strip() for line in line2_candidates.split('\n')]
+	loss_scores = []
+	for candidate in line2_candidates:
+		combined = inputs['line1'] + ' ' + candidate      # may be better to concatenate *after* tokenization using special [SEP] token
+		input_tokens = tokenizer.encode(combined, add_special_tokens=True)
+		input_ids = torch.tensor(input_tokens).unsqueeze(0)
+		outputs = model(input_ids)
+		sequence_loss = outputs[0][0][0]     # outputs is an array with losses as the first value and logits as the second
+		#sequence_loss = outputs[0][0]	# not sure what all the nested levels are here
+		sequence_loss = float(sequence_loss.cpu().detach().numpy())
+		loss_scores.append(sequence_loss)
+  return loss_scores
 
 if __name__ == '__main__':
     runway.run()
